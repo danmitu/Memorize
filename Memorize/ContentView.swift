2c9eb0d3acdb0fbcd1f15da1242748b13469c296
Dan Mitu <contact@danmitu.me>
2020-05-20 15:55:57 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Dan Mitu on 5/20/20.
+//  Copyright © 2020 Dan Mitu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
